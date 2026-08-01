c941b8123d996f7ff46446224705f96c1d0a8488
Spagles <106791090+Spagles@users.noreply.github.com>
2026-08-01 18:51:27 +1000

    fix: catch async errors in API authorization middleware

--- src/Router/ApiRoutes/Middlewares/AuthorizationMiddleware.ts
+++ src/Router/ApiRoutes/Middlewares/AuthorizationMiddleware.ts
@@ -17,16 +17,20 @@ const INVALID_OR_EXPIRED_TOKEN: ApiErrTemplate = {
 };
 
 export default async function handleApiAuthorization(req: Express.Request, res: Express.Response, next: Express.NextFunction): Promise<void> {
-  const accessToken = Tokens.extractBearerToken(req);
-  if (accessToken == null || !Tokens.checkTokenSyntax(accessToken)) {
-    return next(ApiError.create(MISSING_OR_INVALID_TOKEN_SYNTAX));
-  }
+  try {
+    const accessToken = Tokens.extractBearerToken(req);
+    if (accessToken == null || !Tokens.checkTokenSyntax(accessToken)) {
+      return next(ApiError.create(MISSING_OR_INVALID_TOKEN_SYNTAX));
+    }
 
-  const grant = await db.getActiveGrantForAccessToken(accessToken);
-  if (grant == null) {
-    return next(ApiError.create(INVALID_OR_EXPIRED_TOKEN));
-  }
+    const grant = await db.getActiveGrantForAccessToken(accessToken);
+    if (grant == null) {
+      return next(ApiError.create(INVALID_OR_EXPIRED_TOKEN));
+    }
 
-  res.locals.grant = grant;
-  next();
+    res.locals.grant = grant;
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
